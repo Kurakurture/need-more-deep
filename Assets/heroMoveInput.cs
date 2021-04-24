96d4d933077fbf4ef94605994f5e4a5abcc2c4a9
kurakurture@gmail.com <Dima Ulitin>
2021-04-24 15:31:46 +0300

    Base level and input + camera

---
+++ Assets/heroMoveInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class heroMoveInput : MonoBehaviour
+{
+    public GameObject referencedPotitionObject;
+    public Rigidbody rb;
+    public int maxVectorForceX;
+    public int minVectorForceX;
+
+    public UnityEvent JumpStart;
+    public UnityEvent MoveToRightStart;
+    public UnityEvent MoveToLefttStart;
+
+    public Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if(rb.velocity.x > maxVectorForceX){rb.velocity = new Vector3(maxVectorForceX,rb.velocity.y,rb.velocity.z);}
+        //if(rb.velocity.x < minVectorForceX){rb.velocity = new Vector3(minVectorForceX,rb.velocity.y,rb.velocity.z);}
+        //if(rb.velocity.z != 0 ){rb.velocity = new Vector3(rb.velocity.x,rb.velocity.y,0);}
+
+        //velocity = rb.velocity;
+
+        //gameObject.transform.LookAt(referencedPotitionObject.transform);
+
+        if (Input.GetKey("a"))
+        {
+            MoveToLefttStart.Invoke();
+        }
+
+        if (Input.GetKey("d"))
+        {
+            MoveToRightStart.Invoke();
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            JumpStart.Invoke();
+        }
+    }
+}
---
+++ Assets/objectRotateToObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class objectRotateToObject : MonoBehaviour
+{
+    public GameObject observeObject;
+    public Vector3 offset;
+    
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + offset.x,observeObject.transform.position.y + offset.y,this.transform.position.z+offset.z);
+        //this.transform.LookAt(observeObject.transform);
+    }
+}
